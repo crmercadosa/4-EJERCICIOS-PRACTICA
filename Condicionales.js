2dbf40175426049c9dd2d529fa12741830e53dfe
CrissCross20 <165857522+CrissCross20@users.noreply.github.com>
2024-08-31 14:47:46 -0700

    Ejercicios Basicos en Javascript

---
+++ Condicionales.js
@@ -0,0 +1,8 @@
+function mayorDeEdad(edad){
+if (edad>17) {
+    console.log("Eres mayor de edad")
+}else{
+    console.log("Eres menor de edad")
+}
+}
+mayorDeEdad(16);
---
+++ Operaciones.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 3;
+
+let suma = a + b;
+let resta = a - b;
+let mul = a * b;
+let div = a / b;
+
+console.log("La suma de "+a+" y "+b+" es "+suma);
+console.log("La resta de "+a+" y "+b+" es "+resta);
+console.log("La multiplicación de "+a+" y "+b+" es "+mul);
+console.log("La division de "+a+" y "+b+" es "+div);
